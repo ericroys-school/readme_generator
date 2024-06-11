492088217decd731dd6dadcbbdb19cea4e01d3a3
eric.roys@gmail.com <eric.roys@gmail.com>
2024-06-11 17:24:39 -0500

    cleanup and add todo to missing vals

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 import { getAnswers } from "./lib/questions.js";
 import { QA_MAP } from "./lib/model.js";
 import { writer } from "./lib/writer.js";
-import { getLicenseUrl } from "./lib/license_options.js";
+import { getLicenseUrl } from "./lib/licenseOptions.js";
 
-//Output a message to the user about the progtam and
+//clear screen of any residuals
+console.clear();
+
+//Output a message to the user about the program and
 // where the output can be found
-const initMsg = `${"*".repeat(60)}
+console.log(`${"*".repeat(60)}
 The readme generator will automatically create
 a readme.md file for you by asking questions and 
 then utilizing your responses. The new readme.md 
 file will be written into the output directory
-${"*".repeat(60)}\n\n`;
+${"*".repeat(60)}\n\n`);
 
-//clear screen of any residuals
-console.clear();
-//output the message
-console.log(initMsg);
 //call Q & A because not all answers are 42
 let answers = await getAnswers();
+//fill the missing blanks with todo
+addToDo(answers);
 //clear screen of Q&A
 console.clear();
 // console.log(answers);
@@ -28,7 +29,11 @@ let { TITLE, DESC, TOC, INST, USAGE, LIC, CONT, TEST, QA, EMAIL, ID } = QA_MAP;
 //get the html url for adding license link
 const lic_uri = await getLicenseUrl(answers[LIC.name].uri);
 
-//write all the content using the decorated writer
+/* write all the content using the decorated writer
+// not entirely happy with the design as might have been better to
+// create a template file and do some iteration/replacement since
+// it would have small file footprint. Maybe next time :-)
+*/
 const r = writer();
 r.write(`# ${answers[TITLE.name]}`)
   .write(
@@ -76,11 +81,26 @@ r.write(`# ${answers[TITLE.name]}`)
   .write(`## ${QA.label}`)
   .write("")
   .write(`[Contact via Email](mailto:${answers[EMAIL.name]})`)
+  .write("")
   .write(
-    `See this an other projects by stalking my [GitHub]("https://github.com/${
+    `See this and other projects by stalking my [GitHub]("https://github.com/${
       answers[ID.name]
     })`
   );
+
+  console.log("Please review your new readme in the output directory");
+
+//add the Todo to any missing vals
+function addToDo(obj){
+    
+    Object.keys(obj).map(o => {
+        let _t = obj[o];
+        if(typeof _t == 'string' && _t.length === 0){
+            console.log(_t.length);
+            obj[o] = "TODO:Add content here";
+        }
+    })
+}
 
 //non words to space
 function fixBadge(a) {
